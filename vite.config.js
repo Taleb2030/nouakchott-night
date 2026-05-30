0595eb2409e78c060e91f21320a153923f0e4b1c
alibyh <alietud79@gmail.com>
2026-05-30 15:18:26 +0700

    Fix Vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // GitHub Pages: https://taleb2030.github.io/nouakchott-night/
 export default defineConfig({
-  base: '/nouakchott-night/',
+  base: '/',
   plugins: [react()],
 })
